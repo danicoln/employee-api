657147583ee79941baf1ae10a92543876f2efdb6
Daniel Lincoln <lincolndeveloper@outlook.com>
2023-04-27 16:42:12 -0300

    first commit

---
+++ src/main/java/domain/model/Funcionario.java
@@ -0,0 +1,102 @@
+package domain.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+public class Funcionario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cargoTitulo;
+    private String telefone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, String email, String cargoTitulo, String telefone, String imageUrl, String employeeCode) {
+        this.nome = nome;
+        this.email = email;
+        this.cargoTitulo = cargoTitulo;
+        this.telefone = telefone;
+        this.imageUrl = imageUrl;
+        this.employeeCode = employeeCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCargoTitulo() {
+        return cargoTitulo;
+    }
+
+    public void setCargoTitulo(String cargoTitulo) {
+        this.cargoTitulo = cargoTitulo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", cargoTitulo='" + cargoTitulo + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
+}
